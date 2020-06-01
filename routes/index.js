d85d63548c3c5377911bc8dc0acc1bc8b57db20c
Sergey Leonov <s.leonov@fbs.com>
2020-06-01 13:36:38 +0300

    adding basic controller

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const storeController = require('../controllers/storeController');
+
 // Do work here
-router.get('/', (req, res) => {
-  const name = {name: "Kappa", age: "200", comeFact: "yes"};
-  // res.json(name);
-  // res.send('Hey! It works!');
-
-  //If u wanna get smthn from url, use this:
-  // res.send(req.query.name);
-  // res.json(req.query);
-  res.render('main', {
-    name: "Sergei",
-    dog: "No dog :(",
-    randomStatement: req.query.statement,
-    title: "I am node developer"
-  });
-});
-
-router.get('/reverse/:name', ((req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-}));
+router.get('/',storeController.homePage);
 
 module.exports = router;
