eb52dd5b6ce5000de5efc5b3674c35db5027ac35
Sergey Leonov <s.leonov@fbs.com>
2020-06-01 09:19:27 +0300

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+// Do work here
+router.get('/', (req, res) => {
+  const name = {name: "Kappa", age: "200", comeFact: "yes"};
+  // res.json(name);
+  // res.send('Hey! It works!');
+
+  //If u wanna get smthn from url, use this:
+  // res.send(req.query.name);
+  // res.json(req.query);
+  res.render('main', {
+    name: "Sergei",
+    dog: "No dog :(",
+    randomStatement: req.query.statement
+  });
+});
+
+router.get('/reverse/:name', ((req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+}));
+
+module.exports = router;
