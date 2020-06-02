d8acd2a28bfde8064fd34c8f4fe4075e5c2321f1
Sergey Leonov <s.leonov@fbs.com>
2020-06-02 12:51:13 +0300

    adding store schema

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,12 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'Sergei';
-    res.cookie('name', 'Sergei', { maxAge: 900 });
-    if (req.name === 'Sergei') {
-        throw Error('That is a stupid name')
-    }
-    next();
-};
-
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('index');
